5c07d8cfc7b7c581795e7429deb4d048e9180291
Olehgan <o.efimov36@mail.ru>
2022-01-11 22:34:34 +0300

    --

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import {SuperButton} from "../common/SuperButton/SuperButton";
+import {useDispatch} from "react-redux";
+import {registerTC} from "../../reducers/auth";
 
 export type RegisterTypeProps = {}
 
 export const Register: React.FC = () => {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const onClickRegister = () => {
+        const data = {
+            email: email,
+            password: password,
+        }
+        dispatch(registerTC(data))
+    }
     return (
    <div>
-       register
+       <SuperButton onClick={onClickRegister}>Login</SuperButton>
    </div>
     )
 
--- src/reducers/auth.tsx
+++ src/reducers/auth.tsx
@@ -1,9 +1,14 @@
+import {authApi, LoginParamsType} from "../api/login-api";
+import {Dispatch} from "react";
+import {ActionsType} from "./loginReducer";
+import {registerApi, RegisterParamsType} from "../api/register-api";
+
 const initialState = {
   
 }
 type InitialStateType = typeof initialState
 
-export const authReducer = (state:InitialStateType = initialState, action: ActionsType):InitialStateType => {
+export const authReducer = (state:InitialStateType = initialState, action: AuthActionsType):InitialStateType => {
   switch (action.type) {
     default:
       return state
@@ -11,4 +16,13 @@ export const authReducer = (state:InitialStateType = initialState, action: Actio
 };
 
 
-type ActionsType = any
+export const registerTC = (data: RegisterParamsType) => (dispatch: Dispatch<AuthActionsType>) => {
+
+  registerApi.register(data)
+      .then((res) => {
+      debugger
+        console.log(res)
+      })
+
+}
+type AuthActionsType = any
