8d3b56479fdb0d1e5b03aabfa405c889c213e1d6
Olehgan <o.efimov36@mail.ru>
2022-01-11 17:20:20 +0300

    creat Register.tsx component, added registration form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { NavLink, Route, Routes } from "react-router-dom";
 import {SuperInputText} from './components/common/SuperInputText/SuperInputText';
 import {SuperButton} from './components/common/SuperButton/SuperButton';
 import {SuperCheckbox} from './components/common/SuperCheckbox/SuperCheckbox';
+import {Register} from "./components/Register/Register";
+import axios from "axios";
 
 function App() {
+
   return (
     <div className="App">
       <div>
@@ -22,7 +25,7 @@ function App() {
 
         <Route path={"/"} element={<div>Main</div>} />
         <Route path={"/login"} element={<div>login</div>} />
-        <Route path={"/registration"} element={<div>registration</div>} />
+        <Route path={"/registration"} element={<Register/>} />
         <Route path={"/profile"} element={<div>profile</div>} />
         <Route path={"/restore"} element={<div>restore</div>} />
         <Route path={"/newPass"} element={<div>newPass</div>} />
