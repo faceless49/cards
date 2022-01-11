1385517c9e5175f02a8c75c2738ec20d63cf7db6
LiubovSchmit <liubou.schmit@gmail.com>
2022-01-11 17:09:17 +0100

    creating loginReducer.ts

--- src/api/login-api.ts
+++ src/api/login-api.ts
@@ -8,9 +8,9 @@ export const authApi = {
     /*    me() {
             return instance.get<CommonResponseType<{id: number, email: string, login:string}>>('auth/me')
         },*/
-    logout(){
+  /*  logout(){
         return instance.delete<CommonResponseType<{userId?:number}>>('auth/me')
-    }
+    }*/
 }
 
 
@@ -20,12 +20,13 @@ export const authApi = {
 export type LoginParamsType = {
     email: string;
     password: string;
-    captcha?: string;
+    rememberMe:boolean;
+
 }
 
 export type CommonResponseType<T = {}> = {
-    resultCode: number
+   /* resultCode: number
     messages: Array<string>
     fieldsErrors: Array<string>
-    data: T
+    data: T*/
 }
