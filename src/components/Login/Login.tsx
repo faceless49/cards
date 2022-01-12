d6cdd51883b2d0ac1adc4c7b4c58ff8b4d086949
LiubovSchmit <liubou.schmit@gmail.com>
2022-01-12 13:59:19 +0100

    refactoring

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,77 +1,81 @@
-import React, {ChangeEvent, useState} from 'react'
-import './login.module.css';
-import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType} from "../../redux/store";
-import {SuperButton} from "../common/SuperButton/SuperButton";
-import {SuperInputText} from "../common/SuperInputText/SuperInputText";
-import {loginTC, setIsLoggedInAC} from "../../reducers/loginReducer";
+import React, { ChangeEvent, useState } from "react";
+import "./login.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { AppRootStateType } from "../../redux/store";
+import { SuperButton } from "../common/SuperButton/SuperButton";
+import { SuperInputText } from "../common/SuperInputText/SuperInputText";
+import { loginTC, setIsLoggedInAC } from "../../reducers/loginReducer";
 
 export const Login = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    /*const storeIsLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn)*/
-    /* const storeUsername = useSelector<AppRootStateType, string>(state => state.login.username)
+  /*const storeIsLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn)*/
+  /* const storeUsername = useSelector<AppRootStateType, string>(state => state.login.username)
      const storePassword = useSelector<AppRootStateType, string>(state => state.login.password)*/
 
+  /*    const [value, setValue] = useState<boolean>(storeIsLoggedIn)*/
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    /*    const [value, setValue] = useState<boolean>(storeIsLoggedIn)*/
-    const [username, setUsername] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
+  const onChangeUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.currentTarget.value);
+  };
 
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
 
-    const onChangeUsername =
-        (e: ChangeEvent<HTMLInputElement>) => {
-            setUsername(e.currentTarget.value)
-        }
+  const onClickForgotPassword = () => {
+    //todo create redirects
+    return console.log("redirect");
+  };
+  const onClickSignUp = () => {
+    // todo create redirects
+    return console.log("redirect");
+  };
 
-    const onChangePassword =
-        (e: ChangeEvent<HTMLInputElement>) => {
-            setPassword(e.currentTarget.value)
-        }
+  const onClickLogin = () => {
+    const data = {
+      email: username,
+      password: password,
+      rememberMe: true, //  todo checkbox RememberMe create
+    };
+    dispatch(loginTC(data));
+  };
 
-    const onClickForgotPassword = () => {
-        return console.log('redirect')
-    }
-    const onClickSignUp = () => {
-        return console.log('redirect')
-    }
+  return (
+    <div className="login-wrapper">
+      <h1>It-Incubator</h1>
 
-    const onClickLogin = () => {
-        const data = {
-            email: username,
-            password: password,
-            rememberMe: true, //  todo checkbox create
-        }
-        dispatch(loginTC(data))
-    }
+      <h2>Sign In</h2>
 
-    return (<div className="login-wrapper">
+      <div>
+        <p>Username</p>
+        <SuperInputText value={username} onChange={onChangeUsername} />
+        <p>Password</p>
+        <SuperInputText
+          type={"password"}
+          value={password}
+          onChange={onChangePassword}
+        />
+      </div>
 
-        <h1>It-Incubator</h1>
+      <div>
+        <SuperButton onClick={onClickForgotPassword}>
+          {" "}
+          Forgot password
+        </SuperButton>
+      </div>
 
-        <h2>Sign In</h2>
+      <div>
+        <SuperButton onClick={onClickLogin}>Login</SuperButton>
+      </div>
 
-        <div>
-            <p>Username</p>
-            <SuperInputText value={username} onChange={onChangeUsername}/>
-            <p>Password</p>
-            <SuperInputText type={'password'} value={password} onChange={onChangePassword}/>
-        </div>
+      <p>Don't have an account?</p>
 
-        <div>
-            <SuperButton onClick={onClickForgotPassword}> Forgot password</SuperButton>
-        </div>
-
-        <div>
-            <SuperButton onClick={onClickLogin}>Login</SuperButton>
-        </div>
-
-        <p>Don't have an account?</p>
-
-        <div>
-            <SuperButton onClick={onClickSignUp}>Sign Up</SuperButton>
-        </div>
-
-    </div>)
-}
-
+      <div>
+        <SuperButton onClick={onClickSignUp}>Sign Up</SuperButton>
+      </div>
+    </div>
+  );
+};
