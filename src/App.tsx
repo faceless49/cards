586b1e0749f86f12b67f4cebdcdf3b0a130cebda
faceless49 <digital.zeus@mail.ru>
2022-01-13 21:28:39 +0300

    merge 13.01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from "react";
-import {NavLink, Route, Routes} from "react-router-dom";
-import {SuperInputText} from "./components/common/SuperInputText/SuperInputText";
-import {SuperButton} from "./components/common/SuperButton/SuperButton";
-import {SuperCheckbox} from "./components/common/SuperCheckbox/SuperCheckbox";
+import React, {useEffect} from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+import {SuperInputText} from './components/common/SuperInputText/SuperInputText';
+import {SuperButton} from './components/common/SuperButton/SuperButton';
+import {SuperCheckbox} from './components/common/SuperCheckbox/SuperCheckbox';
+<<<<<<<<< Temporary merge branch 1
+import {Register} from "./components/Register/Register";
+import axios from "axios";
+=========
 import {Login} from "./components/Login/Login";
 import {RegistrationContainer} from "./components/Register/RegisterContainer";
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <NavLink to={"/"}>Main</NavLink>---
-                <NavLink to={"/login"}>Login</NavLink>---
-                <NavLink to={"/registration"}>Form Registration</NavLink>---
-                <NavLink to={"/profile"}>Profile</NavLink>---
-                <NavLink to={"/restore"}>restore</NavLink>---
-                <NavLink to={"/newPass"}>newPass</NavLink>---
-                <NavLink to={"/restore"}>restore</NavLink>---
-                <NavLink to={"/test"}>Test</NavLink>
-            </div>
-            <Routes>
-                <Route path={"/*"} element={<div>404</div>}/>
+
+  return (
+    <div className="App">
+      <div>
+        <NavLink to={"/"}>Main</NavLink>---
+        <NavLink to={"/login"}>Login</NavLink>---
+        <NavLink to={"/registration"}>Form Registration</NavLink>---
+        <NavLink to={"/profile"}>Profile</NavLink>---
+        <NavLink to={"/restore"}>restore</NavLink>---
+        <NavLink to={"/newPass"}>newPass</NavLink>---
+        <NavLink to={"/restore"}>restore</NavLink>---
+        <NavLink to={"/test"}>Test</NavLink>
+      </div>
+      <Routes>
+        <Route path={"/*"} element={<div>404</div>} />
+
 
 
                 <Route path={"/"} element={<div>Main</div>}/>
