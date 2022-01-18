72bf447c860a7fa0f09ed6ce101ba737175e7c2b
AndreyTverdovskiy <tverdovskiyandrey1996@gmail.com>
2022-01-18 20:58:40 +0300

    Add LogOut

--- src/reducers/loginReducer.ts
+++ src/reducers/loginReducer.ts
@@ -1,60 +1,76 @@
-import { Dispatch } from "react";
-import { authApi, LoginParamsType } from "../api/login-api";
-import { setProfileData } from "./profile";
+import {authApi, LoginParamsType} from "../api/login-api";
+import {setProfileData, setProfileDeleteData, setProfileError} from "./profile";
+import {Dispatch} from "redux";
+import axios from "axios";
 
 export const initialState: InitialStateType = {
-  isLoggedIn: false,
-  error: null,
+    isLoggedIn: false,
+    error: null,
 };
 
 export const loginReducer = (
-  state: InitialStateType = initialState,
-  action: ActionsType
+    state: InitialStateType = initialState,
+    action: ActionsType
 ): InitialStateType => {
-  switch (action.type) {
-    case "login/SET-IS-LOGGED-IN":
-      return { ...state, isLoggedIn: action.value };
-    case "login/SET-ERROR":
-      return { ...state, error: action.error };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "login/SET-IS-LOGGED-IN":
+            return {...state, isLoggedIn: action.value};
+        case "login/SET-ERROR":
+            return {...state, error: action.error};
+        default:
+            return state;
+    }
 };
 
 // action creators
 export const setErrorAC = (error: string | null) =>
-  ({ type: "login/SET-ERROR", error } as const);
+    ({type: "login/SET-ERROR", error} as const);
 export const setIsLoggedInAC = (value: boolean) =>
-  ({ type: "login/SET-IS-LOGGED-IN", value } as const);
+    ({type: "login/SET-IS-LOGGED-IN", value} as const);
 
 //thunks creators
 
 export const loginTC =
-  (data: LoginParamsType) => (dispatch: Dispatch<ActionsType>) => {
-    authApi
-      .login(data)
-      .then((res) => {
-        dispatch(setIsLoggedInAC(true));
-        dispatch(setProfileData(res.data));
-      })
-      .catch((err) => {
-        const error = err.response
-          ? err.response.data.error
-          : err.message + " , more details in console";
-        console.log("Error: ", { ...err });
+    (data: LoginParamsType) => (dispatch: Dispatch) => {
+        authApi
+            .login(data)
+            .then((res) => {
+                dispatch(setIsLoggedInAC(true));
+                dispatch(setProfileData(res.data));
+            })
+            .catch((err) => {
+                const error = err.response
+                    ? err.response.data.error
+                    : err.message + " , more details in console";
+                console.log("Error: ", {...err});
 
-        dispatch(setErrorAC(error));
-      });
-  };
+                dispatch(setErrorAC(error));
+            });
+    };
+
+export const logOut = () => async (dispatch:Dispatch) => {
+  try {
+    await authApi.LogOut();
+    dispatch(setProfileDeleteData());
+    dispatch(setIsLoggedInAC(false))
+
+  } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+          dispatch(setProfileError(err.response.data.error))
+      }
+  } finally {
+      // dispatch loading (false)
+  }
+}
 
 //types
 
 export type ActionsType =
-  | ReturnType<typeof setIsLoggedInAC>
-  | ReturnType<typeof setErrorAC>
-  | ReturnType<typeof setProfileData>;
+    | ReturnType<typeof setIsLoggedInAC>
+    | ReturnType<typeof setErrorAC>
+    | ReturnType<typeof setProfileData>;
 
 type InitialStateType = {
-  isLoggedIn: boolean;
-  error: string | null;
+    isLoggedIn: boolean;
+    error: string | null;
 };
