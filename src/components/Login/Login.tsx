8e17b8fd44c6b5584145113e84888b78b019156e
faceless49 <digital.zeus@mail.ru>
2022-01-18 20:08:54 +0300

    fix button disabled

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,8 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 // @ts-ignore
-import s from '../../styles/common/AuthStyles.module.scss';
+import s from "../../styles/common/AuthStyles.module.scss";
 // @ts-ignore
-import style from './Login.module.scss';
+import style from "./Login.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { AppRootStateType } from "../../redux/store";
 import { SuperButton } from "../common/SuperButton/SuperButton";
@@ -13,7 +13,7 @@ import Title from "../common/Title/Title";
 import Subtitle from "../common/Subtitle/Subtitle";
 import { SuperCheckbox } from "../common/SuperCheckbox/SuperCheckbox";
 // @ts-ignore
-import eyeIcon from '../../images/eyeIcon.svg';
+import eyeIcon from "../../images/eyeIcon.svg";
 
 export const Login = React.memo(() => {
   const dispatch = useDispatch();
@@ -64,9 +64,6 @@ export const Login = React.memo(() => {
     }
   }, [isLoggedIn]);
 
-  const onClickSignUp = () => {
-    return navigate("/registration");
-  };
 
   return (
     <div className={s.AuthShape} style={{ paddingTop: "30px" }}>
@@ -74,50 +71,73 @@ export const Login = React.memo(() => {
       <Subtitle subtitle="Sign in" />
       <div>{error}</div>
       {status === "loading" && <span>{status}</span>}
-    <form className={style.FormBox}>
-      <div className={s.formWrap} style={{ textAlign: "left", marginBottom: "38px" }}>
-        <label className={s.InputLabel}>Email</label>
-        <SuperInputText
-          style={{width:"100%",marginBottom:"24px"}}
-          value={username}
-          disabled={disabled}
-          placeholder="j&johnson@gmail.com"
-          onChange={onChangeUsername} />
-        <label className={s.InputLabel}>Password</label>
-        <SuperInputText
-          style={{ width: "100%", marginBottom: "5px" }}
-          type={eye}
-          value={password}
-          disabled={disabled}
-          placeholder="*********"
-          onChange={onChangePassword}
-        />
+      <form className={style.FormBox}>
+        <div
+          className={s.formWrap}
+          style={{ textAlign: "left", marginBottom: "38px" }}
+        >
+          <label className={s.InputLabel}>Email</label>
+          <SuperInputText
+            style={{ width: "100%", marginBottom: "24px" }}
+            value={username}
+            disabled={disabled}
+            placeholder="j&johnson@gmail.com"
+            onChange={onChangeUsername}
+          />
+          <label className={s.InputLabel}>Password</label>
+          <SuperInputText
+            style={{ width: "100%", marginBottom: "5px" }}
+            type={eye}
+            value={password}
+            disabled={disabled}
+            placeholder="*********"
+            onChange={onChangePassword}
+          />
 
-    <div className={style.psRelative}>
-      <button className={style.EyeButton} onClick={openEye}><img src={eyeIcon}/></button>
-    </div>
+          <div className={style.psRelative}>
+            <button className={style.EyeButton} onClick={openEye}>
+              <img src={eyeIcon} />
+            </button>
+          </div>
 
-
-
-        <div style={{display:"flex", flexDirection:"column", alignItems:"flex-start", gap:"10px"}}>
-
-        <SuperCheckbox onChange={rememberMeHandler}> Remember me</SuperCheckbox>
-        <NavLink className={s.LinkBasic} to={"/restore"} style={{marginLeft:"0"}}>
-          Forgot Password</NavLink>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              gap: "10px",
+            }}
+          >
+            <SuperCheckbox onChange={rememberMeHandler}>
+              Remember me
+            </SuperCheckbox>
+            <NavLink
+              className={s.LinkBasic}
+              to={"/restore"}
+              style={{ marginLeft: "0" }}
+            >
+              Forgot Password
+            </NavLink>
+          </div>
         </div>
 
-      </div>
-
-        <SuperButton onClick={onClickLogin} style={{ marginTop: "65px", marginBottom:"30px"}}>
+        <SuperButton
+          onClick={onClickLogin}
+          style={{ marginTop: "65px", marginBottom: "30px" }}
+          disabled={disabled}
+        >
           Login
         </SuperButton>
 
-      <div className={style.linkWrap}>
-        <span className={style.textLight} style={{marginBottom:"10px"}}>Don't have an account?</span>
-        <NavLink className={s.LinkActive} to={"/registration"}>Sign Up</NavLink>
-      </div>
-    </form>
-
+        <div className={style.linkWrap}>
+          <span className={style.textLight} style={{ marginBottom: "10px" }}>
+            Don't have an account?
+          </span>
+          <NavLink className={s.LinkActive} to={"/registration"}>
+            Sign Up
+          </NavLink>
+        </div>
+      </form>
     </div>
   );
 });
