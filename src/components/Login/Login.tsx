6b262106310b0a4f49a1c4b4071916aca475d11b
Olehgan <o.efimov36@mail.ru>
2022-01-20 16:13:43 +0300

    --

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,114 +1,114 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 // @ts-ignore
 import s from '../../styles/common/AuthStyles.module.scss';
 // @ts-ignore
 import style from './Login.module.scss';
-import { useDispatch, useSelector } from "react-redux";
-import { AppRootStateType } from "../../redux/store";
-import { SuperButton } from "../common/SuperButton/SuperButton";
-import { SuperInputText } from "../common/SuperInputText/SuperInputText";
-import { loginTC } from "../../reducers/loginReducer";
-import { NavLink, useNavigate } from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../../redux/store";
+import {SuperButton} from "../common/SuperButton/SuperButton";
+import {SuperInputText} from "../common/SuperInputText/SuperInputText";
+import {loginTC} from "../../reducers/loginReducer";
+import {NavLink, useNavigate} from "react-router-dom";
 import Title from "../common/Title/Title";
 import Subtitle from "../common/Subtitle/Subtitle";
-import { SuperCheckbox } from "../common/SuperCheckbox/SuperCheckbox";
+import {SuperCheckbox} from "../common/SuperCheckbox/SuperCheckbox";
 
 export const Login = React.memo(() => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  const isLoggedIn = useSelector<AppRootStateType, boolean>(
-    (state) => state.login.isLoggedIn
-  );
-  const error = useSelector<AppRootStateType, string | null>(
-    (state) => state.login.error
-  );
-  const status = useSelector<AppRootStateType, string>(
-    (state) => state.login.status
-  );
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(
+        (state) => state.login.isLoggedIn
+    );
+    const error = useSelector<AppRootStateType, string | null>(
+        (state) => state.login.error
+    );
+    const status = useSelector<AppRootStateType, string>(
+        (state) => state.login.status
+    );
 
-  const disabled = status === "loading";
+    const disabled = status === "loading";
 
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [checked, setChecked] = useState<boolean>(false);
-  const [eye, setEye] = useState<string>("password");
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [checked, setChecked] = useState<boolean>(false);
+    const [eye, setEye] = useState<string>("password");
 
-  const onChangeUsername = (e: ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.currentTarget.value);
-  };
-  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.currentTarget.value);
-  };
-  const openEye = () => {
-    setEye(eye === "password" ? "text" : "password");
-  };
-  const rememberMeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setChecked(e.currentTarget.checked);
-  };
-
-  const onClickLogin = () => {
-    const data = {
-      email: username,
-      password: password,
-      rememberMe: checked,
+    const onChangeUsername = (e: ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.currentTarget.value);
     };
-    dispatch(loginTC(data));
-  };
+    const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value);
+    };
+    const openEye = () => {
+        setEye(eye === "password" ? "text" : "password");
+    };
+    const rememberMeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setChecked(e.currentTarget.checked);
+    };
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate("/profile");
-    }
-  }, [isLoggedIn]);
+    const onClickLogin = () => {
+        const data = {
+            email: username,
+            password: password,
+            rememberMe: checked,
+        };
+        dispatch(loginTC(data));
+    };
 
-  const onClickSignUp = () => {
-    return navigate("/registration");
-  };
+    useEffect(() => {
+        if (isLoggedIn) {
+            navigate("/profile");
+        }
+    }, [isLoggedIn]);
 
-  return (
-    <div className={s.AuthShape} style={{ paddingTop: "30px" }}>
-      <Title />
-      <Subtitle subtitle="Sign in" />
-      <div>{error}</div>
-      {status === "loading" && <span>{status}</span>}
-    <form className={style.FormBox}>
-      <div className={s.formWrap} style={{ textAlign: "left", marginBottom: "38px" }}>
-        <label className={s.InputLabel}>Email</label>
-        <SuperInputText
-          style={{width:"100%",marginBottom:"24px"}}
-          value={username}
-          disabled={disabled}
-          placeholder="j&johnson@gmail.com"
-          onChange={onChangeUsername} />
-        <label className={s.InputLabel}>Password</label>
-        <SuperInputText
-          style={{ width: "100%", marginBottom: "5px" }}
-          type={eye}
-          value={password}
-          disabled={disabled}
-          placeholder="*********"
-          onChange={onChangePassword}
-        />
+    const onClickSignUp = () => {
+        return navigate("/registration");
+    };
 
-        <div style={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-        <SuperCheckbox onChange={rememberMeHandler}> Remember me</SuperCheckbox>
-        <NavLink className={s.LinkBasic} to={"/restore"} style={{marginLeft:"0", paddingTop:"5px"}}>
-          Forgot Password</NavLink>
+    return (
+        <div className={s.AuthShape} style={{paddingTop: "30px"}}>
+            <Title/>
+            <Subtitle subtitle="Sign in"/>
+            <div>{error}</div>
+            {status === "loading" && <span>{status}</span>}
+            <form className={style.FormBox}>
+                <div className={s.formWrap} style={{textAlign: "left", marginBottom: "38px"}}>
+                    <label className={s.InputLabel}>Email</label>
+                    <SuperInputText
+                        style={{width: "100%", marginBottom: "24px"}}
+                        value={username}
+                        disabled={disabled}
+                        placeholder="j&johnson@gmail.com"
+                        onChange={onChangeUsername}/>
+                    <label className={s.InputLabel}>Password</label>
+                    <SuperInputText
+                        style={{width: "100%", marginBottom: "5px"}}
+                        type={eye}
+                        value={password}
+                        disabled={disabled}
+                        placeholder="*********"
+                        onChange={onChangePassword}
+                    />
+
+                    <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                        <SuperCheckbox onChange={rememberMeHandler}> Remember me</SuperCheckbox>
+                        <NavLink className={s.LinkBasic} to={"/restore"} style={{marginLeft: "0", paddingTop: "5px"}}>
+                            Forgot Password</NavLink>
+                    </div>
+
+                </div>
+
+                <SuperButton onClick={onClickLogin} style={{marginTop: "72px", marginBottom: "30px"}}>
+                    Login
+                </SuperButton>
+
+                <div className={style.linkWrap}>
+                    <span className={style.textLight} style={{marginBottom: "10px"}}>Don't have an account?</span>
+                    <NavLink className={s.LinkActive} to={"/registration"}>Sign Up</NavLink>
+                </div>
+            </form>
+
         </div>
-
-      </div>
-
-        <SuperButton onClick={onClickLogin} style={{ marginTop: "72px", marginBottom:"30px"}}>
-          Login
-        </SuperButton>
-
-      <div className={style.linkWrap}>
-        <span className={style.textLight} style={{marginBottom:"10px"}}>Don't have an account?</span>
-        <NavLink className={s.LinkActive} to={"/registration"}>Sign Up</NavLink>
-      </div>
-    </form>
-
-    </div>
-  );
+    );
 });
--- src/reducers/loginReducer.ts
+++ src/reducers/loginReducer.ts
@@ -1,72 +1,72 @@
-import { Dispatch } from "react";
-import { authApi, LoginParamsType } from "../api/login-api";
-import { setProfileData } from "./profile";
+import {Dispatch} from "react";
+import {authApi, LoginParamsType} from "../api/login-api";
+import {setProfileData} from "./profile";
 
 export const initialState: InitialStateType = {
-  isLoggedIn: false,
-  error: null,
-  status: "idle",
+    isLoggedIn: false,
+    error: null,
+    status: "idle",
 };
 
 export const loginReducer = (
-  state: InitialStateType = initialState,
-  action: ActionsType
+    state: InitialStateType = initialState,
+    action: ActionsType
 ): InitialStateType => {
-  switch (action.type) {
-    case "login/SET-IS-LOGGED-IN":
-      return { ...state, isLoggedIn: action.value };
-    case "login/SET-ERROR":
-      return { ...state, error: action.error };
-    case "login/SET-STATUS":
-      return { ...state, status: action.status };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "login/SET-IS-LOGGED-IN":
+            return {...state, isLoggedIn: action.value};
+        case "login/SET-ERROR":
+            return {...state, error: action.error};
+        case "login/SET-STATUS":
+            return {...state, status: action.status};
+        default:
+            return state;
+    }
 };
 
 // action creators
 export const setErrorAC = (error: string | null) =>
-  ({ type: "login/SET-ERROR", error } as const);
+    ({type: "login/SET-ERROR", error} as const);
 export const setIsLoggedInAC = (value: boolean) =>
-  ({ type: "login/SET-IS-LOGGED-IN", value } as const);
+    ({type: "login/SET-IS-LOGGED-IN", value} as const);
 export const setStatusAC = (
-  status: "loading" | "succeeded" | "failed" | "idle"
-) => ({ type: "login/SET-STATUS", status } as const);
+    status: "loading" | "succeeded" | "failed" | "idle"
+) => ({type: "login/SET-STATUS", status} as const);
 
 //thunks creators
 
 export const loginTC =
-  (data: LoginParamsType) => (dispatch: Dispatch<ActionsType>) => {
-    dispatch(setErrorAC(null));
-    dispatch(setStatusAC("loading"));
-    authApi
-      .login(data)
-      .then((res) => {
-        dispatch(setProfileData(res.data));
-        dispatch(setIsLoggedInAC(true));
-        dispatch(setStatusAC("succeeded"));
-      })
-      .catch((err) => {
-        const error = err.response
-          ? err.response.data.error
-          : err.message + " , more details in console";
-        console.log("Error: ", { ...err });
+    (data: LoginParamsType) => (dispatch: Dispatch<ActionsType>) => {
+        dispatch(setErrorAC(null));
+        dispatch(setStatusAC("loading"));
+        authApi
+            .login(data)
+            .then((res) => {
+                dispatch(setProfileData(res.data));
+                dispatch(setIsLoggedInAC(true));
+                dispatch(setStatusAC("succeeded"));
+            })
+            .catch((err) => {
+                const error = err.response
+                    ? err.response.data.error
+                    : err.message + " , more details in console";
+                console.log("Error: ", {...err});
 
-        dispatch(setErrorAC(error));
-        dispatch(setStatusAC("failed"));
-      });
-  };
+                dispatch(setErrorAC(error));
+                dispatch(setStatusAC("failed"));
+            });
+    };
 
 //types
 
 export type ActionsType =
-  | ReturnType<typeof setIsLoggedInAC>
-  | ReturnType<typeof setErrorAC>
-  | ReturnType<typeof setProfileData>
-  | ReturnType<typeof setStatusAC>;
+    | ReturnType<typeof setIsLoggedInAC>
+    | ReturnType<typeof setErrorAC>
+    | ReturnType<typeof setProfileData>
+    | ReturnType<typeof setStatusAC>;
 
 type InitialStateType = {
-  isLoggedIn: boolean;
-  error: string | null;
-  status: "loading" | "succeeded" | "failed" | "idle";
+    isLoggedIn: boolean;
+    error: string | null;
+    status: "loading" | "succeeded" | "failed" | "idle";
 };
